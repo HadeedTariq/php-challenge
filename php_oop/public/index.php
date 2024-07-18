d731fb20422cafbf098f854d7c00e77a71253360
hadeedtariq <computeranalog351@gmail.com>
2024-07-18 12:42:06 +0100

    php_oop: create controller basic functionality

--- php_oop/public/index.php
+++ php_oop/public/index.php
@@ -2,10 +2,12 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use app\core\Application;
+use app\controllers\SiteController;
 
 $app = new Application(dirname(__DIR__));
 
 $app->router->get("/", 'home');
 $app->router->get("/contact", "contact");
+$app->router->post("/contact", [SiteController::class, 'contact']);
 
 $app->run();
